5810a251c1b14b86291717a78413049f693e207b
DhrithiPai <dhhrithipai@gmail.com>
2021-01-16 13:10:36 +0530

    b

---
+++ StringSort.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+import java.util.Arrays; 
+
+
+public class StringSort 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        
+        String str=sc.nextLine();
+        char arr[]=str.toCharArray();
+        Arrays.sort(arr);
+
+        System.out.print("Resultant string : " + new String(arr));
+        sc.close();
+    }
+    
+}
+

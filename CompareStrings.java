16a00fb32d4ab7e64b7c1d59ea42606a28555eb2
DhrithiPai <dhhrithipai@gmail.com>
2021-01-16 13:25:30 +0530

    e

---
+++ CompareStrings.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Compare 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        
+        String s1=sc.nextLine();
+
+        System.out.print("Enter another string : ");
+        
+        String s2=sc.nextLine();
+
+        sc.close();
+        if(s1.equalsIgnoreCase(s2))
+        {
+            System.out.print("Strings are equal");
+
+        }
+        else
+        {
+            System.out.print("Strings are not equal");
+        }
+        
+        
+
+    }
+    
+}
